2e3a41041d675e3e3ff310b098a6e94e06182450
Tom Lingo <tom.lingo@asynchrony.com>
2019-06-12 10:21:42 -0500

    docker compose

--- server/index.js
+++ server/index.js
@@ -53,12 +53,11 @@ app.get('/values/current', async (req, res) => {
 
 app.post('/values', async (req, res) => {
   const index = req.body.index;
-
   if (parseInt(index) > 40) {
     return res.status(422).send('Index too high');
   }
 
-  redisClient.hset('values', index, 'Nothing yet!');
+  redisClient.hset('values', index, 'Nothing shoe!');
   redisPublisher.publish('insert', index);
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
